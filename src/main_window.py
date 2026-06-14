4f3b11c0eae12766cba292f59fc6bc433f56e283
Celestia Ludenberg <ash8820@proton.me>
2026-06-14 20:14:44 +0200

    fix: replace user_info GtkGrid with GtkBox layout to prevent 2713px auto-position overflow

--- src/main_window.py
+++ src/main_window.py
@@ -265,13 +265,6 @@ class MainWindow(Gtk.ApplicationWindow):
         # Set window geometry
         self.set_default_size(self._main_window_width, self._main_window_height)
         self.set_resizable(True)
-        # Force window size after it's mapped (set_default_size is just a hint)
-        GLib.idle_add(lambda: (self.resize(self._main_window_width, self._main_window_height), False)[1])
-        GLib.timeout_add(2000, lambda: (logging.info("Window size: def=%dx%d actual=%dx%d",
-            self._main_window_width, self._main_window_height,
-            self.get_width(), self.get_height()), False)[1])
-        GLib.timeout_add(2500, self._log_page_sizes)
-
         # Apply CSS
         style_provider = Gtk.CssProvider()
         style_css = os.path.join(data_dir, "css", "gtk-style.css")
